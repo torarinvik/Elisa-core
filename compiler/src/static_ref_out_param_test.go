7fba3255de127c2561978bf6aca921e01021a758
torarinvik <torarinvik@gmail.com>
2026-05-21 12:19:52 +0200

    Cover heap static ref out parameters

--- compiler/src/static_ref_out_param_test.go
+++ compiler/src/static_ref_out_param_test.go
@@ -24,12 +24,49 @@ func TestRunCLIStaticStringRefOutParamRebindsCallerSlot(t *testing.T) {
 def set_out(out: mutable static u8&) -> void:
 	out <- "D4yla3vx4tY"
 
+def make_static_ref() -> static u8&:
+	return "libkernel"
+
+extern malloc(size: usize) -> mutable heap void&?
+
+def copy_prefix(input: static u8&, len: usize) -> static u8&:
+	raw: mutable heap void&? = malloc(len + 1)
+	if raw == null:
+		return ""
+	out: mutable heap u8& = raw.cast[mutable heap u8&]
+	i: mutable usize = 0
+	while i < len:
+		out[i] <- input[i]
+		i <- i + 1
+	out[len] <- 0
+	return out.cast[static u8&]
+
+def cstr_eq(lhs: u8&?, rhs: u8&?) -> bool:
+	if lhs == null or rhs == null:
+		return lhs == rhs
+	i: mutable usize = 0
+	while lhs[i] != 0 and rhs[i] != 0:
+		if lhs[i] != rhs[i]:
+			return false
+		i <- i + 1
+	return lhs[i] == rhs[i]
+
+def set_out_from_call(out: mutable static u8&) -> void:
+	out <- make_static_ref()
+
+def set_out_from_heap_slice(out: mutable static u8&) -> void:
+	out <- copy_prefix("D4yla3vx4tY#libkernel", 11)
+
 @test
 def static_string_ref_out_param_rebinds_caller_slot() -> void:
 	can Abort.Panic:
 		value: mutable static u8& = ""
 		set_out(value.ref[mutable static u8&])
 		assert_eq(value, "D4yla3vx4tY")
+		set_out_from_call(value.ref[mutable static u8&])
+		assert_eq(value, "libkernel")
+		set_out_from_heap_slice(value.ref[mutable static u8&])
+		assert_true(cstr_eq(value, "D4yla3vx4tY"))
 `, testInclude)
 	if err := os.WriteFile(fixturePath, []byte(src), 0o644); err != nil {
 		t.Fatalf("failed to write static ref out-param fixture: %v", err)
